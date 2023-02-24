eaadf776326c2e258f3c29e76e85b6a7f9ea3c83
EricKweyunga <122126027+EricKweyunga@users.noreply.github.com>
2023-02-24 12:40:01 +0000

    setting up a default route

--- engine/__init__.py
+++ engine/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 from public import public
 
 def create_app():
@@ -6,5 +6,9 @@ def create_app():
 
     # blueprints register here
     app.register_blueprint(public, url_prefix='/portfolio')
+
+    @app.route('/')
+    def index():
+        return redirect(url_for('public.index'))
 
     return app
